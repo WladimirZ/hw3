c2a42dd754be75a0a5cfd81d779f1efbea4e023a
Wladimir <Wladimir.Zholobenko@hhu.de>
2015-11-17 00:41:27 +0100

    done

--- statistics.cxx
+++ statistics.cxx
@@ -3,16 +3,31 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+void fill (double* x, int N){
+    for (int i = 0; i<N; i++)
+      x[i] = 1.0*rand()/RAND_MAX;
+}
+
+void meanvar (double& m, double& v, double* x, int N){
+    m = 0;
+    v = 0;
+    for (int i = 0; i<N; i++)
+      m += x[i];
+    m /= N;
+    for (int i = 0; i<N; i++)
+      v += (x[i]-m)*(x[i]-m);
+    v /= N;
+}
 
 int main(){
+   srand(time(NULL));
    const int N = 100;
    double p[N];
    double mean, var;
-
-   // Some lines here....
+   
+   fill (p, N);
+   meanvar (mean, var, p, N);
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
